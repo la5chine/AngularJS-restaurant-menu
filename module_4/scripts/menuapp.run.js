58a751c309740ebac9e64375f48e6a60e282e4b9
chaker khachine <35540842+la5chine@users.noreply.github.com>
2019-07-10 12:29:57 +0100

    commit

---
+++ module_4/scripts/menuapp.run.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular.module('MenuApp')
+    .run(RunFunction);
+
+  RunFunction.$inject = ['$rootScope', '$anchorScroll'];
+
+  
+  function RunFunction($rootScope, $anchorScroll) {
+	 $rootScope.$on('$stateChangeSuccess', function(event, currentState, previousState) {
+		 $anchorScroll();
+	 });
+  }
+})();
